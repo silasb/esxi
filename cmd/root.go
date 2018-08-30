0c644b94dbae91bdd701eb9bc0997cdb3db0e74f
silasb <silas.baronda@gmail.com>
2018-08-29 22:12:19 -0400

    Removing root command hugo examples

--- cmd/root.go
+++ cmd/root.go
@@ -9,10 +9,8 @@ import (
 
 var rootCmd = &cobra.Command{
 	Use:   "",
-	Short: "Hugo is a very fast static site generator",
-	Long: `A Fast and Flexible Static Site Generator built with
-                love by spf13 and friends in Go.
-                Complete documentation is available at http://hugo.spf13.com`,
+	Short: "",
+	Long:  "",
 }
 
 func Execute() {
